f7c877d510409de054dbf0551534cdc3b97c5f55
andreux2025 <turandreu124@gmail.com>
2025-04-05 04:26:34 -0700

    re do

--- internal base by andreu retrac/sdk/sdk.h
+++ internal base by andreu retrac/sdk/sdk.h
@@ -11,159 +11,34 @@
 #include <future>
 #include <winternl.h>
 
-
-namespace Variables
-{
-	SDK2::UCanvas* Canvas = nullptr;
-	SDK2::APlayerController* PlayerController = nullptr;
-	SDK2::APlayerPawn_Athena_C* AcknowledgedPawn = nullptr;
-	SDK2::FName MaterialParameter = SDK2::FName(0);
-	SDK2::FName MaterialParameter2 = SDK2::FName(0);
-	SDK2::FName MaterialParameter3 = SDK2::FName(0);
-	SDK2::FName MaterialParameter4 = SDK2::FName(0);
-	SDK2::FName MaterialParameter5 = SDK2::FName(0);
-	SDK2::FName MaterialParameter6 = SDK2::FName(0);
-
-	const wchar_t* BuildTime = nullptr;
-
-	//Variables
-	static bool DrawMenu = true;
-	static FVector2D ScreenCenter = FVector2D();
-	static FVector2D ScreenSize = FVector2D();
-
-	//Camera
-	static FVector CameraLocation = FVector();
-	static FRotator CameraRotation = FRotator();
-	static float FieldOfView = 0.f;
-
-	//Bullet tracer
-	static int LastBulletIndexPushed = 0;
-	static FVector Last5BulletImpacts[5];
-	static FVector Last5BulletImpactsMuzzle[5];
-
-	//Exploits
-	static FVector BulletTeleportHead = FVector();
-
-	namespace GetDamageStartLocation
-	{
-		static bool bSilentAimActive = false;
-		static FVector SilentLocationTarget = FVector();
-		static FRotator LastTargetRotation = FRotator();
-	}
-
-	namespace GetTargetingTransform
-	{
-		static bool bPickaxeRangeActive = false;
-		static bool bShouldStartAtLocationTarget = false;
-		static FVector LocationTarget = FVector();
-	}
-
-	namespace GetPlayerViewPoint
-	{
-		static bool bShouldStartAtLocationTarget = false;
-		static FVector SilentLocationTarget = FVector();
-		static FRotator LastTargetRotation = FRotator();
-		static FVector OriginalLocation = FVector();
-		static FRotator OriginalRotation = FRotator();
-	}
-	namespace GetCameraViewPoint
-	{
-		static bool bFreecamActive = false;
-
-		static FRotator LastStickyRotation = FRotator();
-		static FVector StickyLocation = FVector();
-		static FVector CachedFreecamLocation = FVector();
-	}
-}
-
-namespace Keys
-{
-	FKey LeftMouseButton;
-	FKey RightMouseButton;
-	FKey Insert;
-	FKey F2;
-	FKey F8;
-	FKey W;
-	FKey A;
-	FKey S;
-	FKey D;
-	FKey SpaceBar;
-	FKey LeftShift;
-	FKey Capslock;
-}
-
-namespace Classes
-{
-	SDK2::UObject* PlayerClass = nullptr;
-	SDK2::UObject* PickupClass = nullptr;
-	SDK2::UObject* ContainerClass = nullptr;
-	SDK2::UObject* ProjectileClass = nullptr;
-	SDK2::UObject* WeakspotClass = nullptr;
-	SDK2::UObject* BuildingTrapClass = nullptr;
-	SDK2::UObject* VehicleClass = nullptr;
-	SDK2::UObject* MarkerClass = nullptr;
-	SDK2::UObject* SupplyDropClass = nullptr;
-	SDK2::UObject* RangedWeaponClass = nullptr;
-	SDK2::UObject* FortWeaponClass = nullptr;
-	SDK2::UObject* PickaxeClass = nullptr;
-	SDK2::UObject* LocalPlayer = nullptr;
-	SDK2::UObject* PlayerController = nullptr;
-	SDK2::UObject* PlayerCameraManager = nullptr;
-}
-
-enum Bones : int {
-	Root = 0,
-	pelvis = 2,
-	spine_01 = 3,
-	spine_02 = 4,
-	spine_03 = 5,
-	spine_04 = 6,
-	spine_05 = 7,
-	clavicle_l = 8,
-	upperarm_l = 9,
-	lowerarm_l = 10,
-	hand_l = 33,
-	clavicle_r = 37,
-	upperarm_r = 38,
-	lowerarm_r = 65,
-	hand_r = 62,
-	neck_01 = 66,
-	neck_02 = 67,
-	head = 68,
-	thigh_l = 71,
-	calf_l = 72,
-	calf_twist_01_l = 73,
-	calf_twist_02_l = 74,
-	foot_l = 76,
-	thigh_r = 78,
-	calf_r = 79,
-	calf_twist_01_r = 80,
-	calf_twist_02_r = 81,
-	foot_r = 83,
-	camera_root = 92,
-	Camera = 93,
-	attach_fp = 94,
-};
-
-const wchar_t* GetBuildTime()
-{
-	const char* BuildTimeA = __TIME__;
-	const int BuildTimeALength = NoCRT::__strlen(BuildTimeA);
-	wchar_t* WideArray = new wchar_t[BuildTimeALength + 1];
-
-	for (int i = 0; i < BuildTimeALength; i++)
-	{
-		WideArray[i] = static_cast<wchar_t>(BuildTimeA[i]);
-	}
-
-	WideArray[BuildTimeALength] = L'\0';
-
-	return WideArray;
-}
-
-
 namespace SDK
 {
+	class UFont : public UObject
+	{
+	public:
+
+	};
+	UObject* KismetMathLibrary = nullptr;
+	UObject* KismetMaterialLibrary = nullptr;
+	UObject* GameplayStatics = nullptr;
+	UObject* FortKismetLibrary = nullptr;
+	UObject* KismetSystemLibrary = nullptr;
+	UObject* KismetStringLibrary = nullptr;
+	UFont* MediumFont = 0;
+
+	class UEngine : public UObject
+	{
+	public:
+		UFont* MediumFont()
+		{
+			return *(UFont**)(this + 0x70);
+		}
+
+		UFont* SmallFont()
+		{
+			return *(UFont**)(this + 0x50);
+		}
+	};
 	// Forward declarations of classes
 	class UWorld;
 	class UScruct;
@@ -350,4 +225,216 @@ namespace SDK
 		}
 	};
 
+	class UCanvas : public UObject
+	{
+	public:
+		float ClipX()
+		{
+			return *(float*)(this + 0x30);
+		}
+
+		float ClipY()
+		{
+			return *(float*)(this + 0x34);
+		}
+
+		void K2_DrawLine(FVector2D ScreenPositionA, FVector2D ScreenPositionB, float Thickness, FLinearColor RenderColor)
+		{
+			static UObject* Function = 0;
+			if (!Function) Function = FindObject(L"Canvas.K2_DrawLine");
+
+			struct {
+				FVector2D ScreenPositionA;
+				FVector2D ScreenPositionB;
+				float Thickness;
+				FLinearColor RenderColor;
+			} Params = { ScreenPositionA, ScreenPositionB, Thickness, RenderColor };
+
+			this->ProcessEvent(Function, &Params);
+		}
+
+		void K2_DrawText(FString RenderText, FVector2D ScreenPosition, FVector2D Scale, FLinearColor RenderColor, float Kerning, bool bCentreX, bool bCentreY, bool bOutlined)
+		{
+			static UObject* Function = 0;
+			if (!Function) Function = FindObject(L"Canvas.K2_DrawText");
+
+			struct {
+				UObject* RenderFont;
+				FString RenderText;
+				FVector2D ScreenPosition;
+				FVector2D Scale;
+				FLinearColor RenderColor;
+				float Kerning;
+				FLinearColor ShadowColor;
+				FVector2D ShadowOffset;
+				bool bCentreX;
+				bool bCentreY;
+				bool bOutlined;
+				FLinearColor OutlineColor;
+			} Params = { MediumFont, RenderText, ScreenPosition, Scale, RenderColor, Kerning, FLinearColor(), FVector2D(), bCentreX, bCentreY, bOutlined, FLinearColor(0.f, 0.f, 0.f, 1.f) };
+
+			this->ProcessEvent(Function, &Params);
+		}
+	};
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+namespace Variables
+{
+	SDK::UCanvas* Canvas = nullptr;
+	//SDK::APlayerController* PlayerController = nullptr;
+	//SDK::APlayerPawn_Athena_C* AcknowledgedPawn = nullptr;
+	SDK2::FName MaterialParameter = SDK2::FName(0);
+	SDK2::FName MaterialParameter2 = SDK2::FName(0);
+	SDK2::FName MaterialParameter3 = SDK2::FName(0);
+	SDK2::FName MaterialParameter4 = SDK2::FName(0);
+	SDK2::FName MaterialParameter5 = SDK2::FName(0);
+	SDK2::FName MaterialParameter6 = SDK2::FName(0);
+
+	const wchar_t* BuildTime = nullptr;
+
+	//Variables
+	static bool DrawMenu = true;
+	static FVector2D ScreenCenter = FVector2D();
+	static FVector2D ScreenSize = FVector2D();
+
+	//Camera
+	static FVector CameraLocation = FVector();
+	static FRotator CameraRotation = FRotator();
+	static float FieldOfView = 0.f;
+
+	//Bullet tracer
+	static int LastBulletIndexPushed = 0;
+	static FVector Last5BulletImpacts[5];
+	static FVector Last5BulletImpactsMuzzle[5];
+
+	//Exploits
+	static FVector BulletTeleportHead = FVector();
+
+	namespace GetDamageStartLocation
+	{
+		static bool bSilentAimActive = false;
+		static FVector SilentLocationTarget = FVector();
+		static FRotator LastTargetRotation = FRotator();
+	}
+
+	namespace GetTargetingTransform
+	{
+		static bool bPickaxeRangeActive = false;
+		static bool bShouldStartAtLocationTarget = false;
+		static FVector LocationTarget = FVector();
+	}
+
+	namespace GetPlayerViewPoint
+	{
+		static bool bShouldStartAtLocationTarget = false;
+		static FVector SilentLocationTarget = FVector();
+		static FRotator LastTargetRotation = FRotator();
+		static FVector OriginalLocation = FVector();
+		static FRotator OriginalRotation = FRotator();
+	}
+	namespace GetCameraViewPoint
+	{
+		static bool bFreecamActive = false;
+
+		static FRotator LastStickyRotation = FRotator();
+		static FVector StickyLocation = FVector();
+		static FVector CachedFreecamLocation = FVector();
+	}
+}
+
+namespace Keys
+{
+	FKey LeftMouseButton;
+	FKey RightMouseButton;
+	FKey Insert;
+	FKey F2;
+	FKey F8;
+	FKey W;
+	FKey A;
+	FKey S;
+	FKey D;
+	FKey SpaceBar;
+	FKey LeftShift;
+	FKey Capslock;
+}
+
+namespace Classes
+{
+	SDK2::UObject* PlayerClass = nullptr;
+	SDK2::UObject* PickupClass = nullptr;
+	SDK2::UObject* ContainerClass = nullptr;
+	SDK2::UObject* ProjectileClass = nullptr;
+	SDK2::UObject* WeakspotClass = nullptr;
+	SDK2::UObject* BuildingTrapClass = nullptr;
+	SDK2::UObject* VehicleClass = nullptr;
+	SDK2::UObject* MarkerClass = nullptr;
+	SDK2::UObject* SupplyDropClass = nullptr;
+	SDK2::UObject* RangedWeaponClass = nullptr;
+	SDK2::UObject* FortWeaponClass = nullptr;
+	SDK2::UObject* PickaxeClass = nullptr;
+	SDK2::UObject* LocalPlayer = nullptr;
+	SDK2::UObject* PlayerController = nullptr;
+	SDK2::UObject* PlayerCameraManager = nullptr;
+}
+
+enum Bones : int {
+	Root = 0,
+	pelvis = 2,
+	spine_01 = 3,
+	spine_02 = 4,
+	spine_03 = 5,
+	spine_04 = 6,
+	spine_05 = 7,
+	clavicle_l = 8,
+	upperarm_l = 9,
+	lowerarm_l = 10,
+	hand_l = 33,
+	clavicle_r = 37,
+	upperarm_r = 38,
+	lowerarm_r = 65,
+	hand_r = 62,
+	neck_01 = 66,
+	neck_02 = 67,
+	head = 68,
+	thigh_l = 71,
+	calf_l = 72,
+	calf_twist_01_l = 73,
+	calf_twist_02_l = 74,
+	foot_l = 76,
+	thigh_r = 78,
+	calf_r = 79,
+	calf_twist_01_r = 80,
+	calf_twist_02_r = 81,
+	foot_r = 83,
+	camera_root = 92,
+	Camera = 93,
+	attach_fp = 94,
+};
+
+const wchar_t* GetBuildTime()
+{
+	const char* BuildTimeA = __TIME__;
+	const int BuildTimeALength = NoCRT::__strlen(BuildTimeA);
+	wchar_t* WideArray = new wchar_t[BuildTimeALength + 1];
+
+	for (int i = 0; i < BuildTimeALength; i++)
+	{
+		WideArray[i] = static_cast<wchar_t>(BuildTimeA[i]);
+	}
+
+	WideArray[BuildTimeALength] = L'\0';
+
+	return WideArray;
 }
